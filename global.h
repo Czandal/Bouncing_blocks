8c877588f1f88483ea30a9d7247c8b22e9e9e32a
Radosław Malec <malec.radoslaw@o2.pl>
2024-02-10 10:11:05 +0100

    Clean up project tree

--- global.h
+++ global.h
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include "SDL2_framerate.h"
 #include "Block.h"
 #include <math.h>
